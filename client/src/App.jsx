3c4125d2c52058bfbb67adc0ac442cb042850b3f
lukeinco <136262607+lukeinco@users.noreply.github.com>
2023-12-05 18:57:58 -0700

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,27 @@
+import './App.css';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+import { Outlet } from 'react-router-dom';
+
+import Header from './components/Header';
+import Footer from './components/Footer';
+
+const client = new ApolloClient({
+  uri: '/graphql',
+  cache: new InMemoryCache(),
+});
+
+function App() {
+  return (
+    <ApolloProvider client={client}>
+      <div>
+        <Header />
+        <div>
+          <Outlet />
+        </div>
+        <Footer />
+      </div>
+    </ApolloProvider>
+  );
+}
+
+export default App;
