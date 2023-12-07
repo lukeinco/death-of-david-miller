ef4dd1f99e9141f6b3a3767fffc87b1e5a576a71
lukeinco <136262607+lukeinco@users.noreply.github.com>
2023-12-07 15:36:13 -0700

    Update Post.js more gpt comments

--- server/models/Post.js
+++ server/models/Post.js
@@ -34,14 +34,19 @@ const postSchema = new Schema({
 });
 
 // GPT Below:
+//const commentSchema = new Schema({
+//  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
+//  commentText: { type: String, required: true },
+//  date: { type: Date, default: Date.now },
+// });
 // const postSchema = new Schema({
 //  userId: { type: Schema.Types.ObjectId, ref: 'User', required: true },
 //  postText: { type: String, required: true },
 //  dateCreated: { type: Date, default: Date.now },
-//  comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
+//  comments: [commentSchema],
 // });
 
-
+//const Comment = model('Comment', commentSchema);
 const Post = model('Post', postSchema);
 
 module.exports = Post;
