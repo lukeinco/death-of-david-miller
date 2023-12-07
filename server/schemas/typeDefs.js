39f6329dbcd84552dbf219df00edb37f44f73f49
lukeinco <136262607+lukeinco@users.noreply.github.com>
2023-12-07 15:53:47 -0700

    Update typeDefs.js gpt comments

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -32,5 +32,71 @@ const typeDefs = `
     removeComment(postId: ID!, commentId: ID!): Post
   }
 `;
+// user.typedefs.js
+//type User {
+//  _id: ID!
+  //username: String!
+  //posts: [Post]!
+//}
+
+//input CreateUserInput {
+  //username: String!
+  //password: String!
+//}
+
+//type AuthPayload {
+  //token: String
+  // user: User
+//}
+
+//type Query {
+  // Add relevant queries for your use case
+//}
+
+//type Mutation {
+ // createUser(input: CreateUserInput): AuthPayload
+ // loginUser(username: String!, password: String!): AuthPayload
+  // Add other user-related mutations if needed
+// }
+
+// post.typedefs.js
+//type Post {
+ // _id: ID!
+ // userId: ID!
+ // postText: String!
+ // dateCreated: String!
+ // comments: [Comment]!
+//}
+
+//input CreatePostInput {
+  //postText: String!
+//}
+
+//type Mutation {
+  //createPost(input: CreatePostInput): Post
+  //deletePost(postId: ID!): Post
+//}
+
+// comment.typedefs.js
+//type Comment {
+  //_id: ID!
+  //userId: ID!
+  //postId: ID!
+  //commentText: String!
+  //date: String!
+//}
+//
+//input CreateCommentInput {
+  //postId: ID!
+  //commentText: String!
+//}
+
+//type Mutation {
+ // createComment(input: CreateCommentInput): Comment
+  //deleteComment(commentId: ID!): Comment
+// }
+
 
 module.exports = typeDefs;
+
+
